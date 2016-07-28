29345dd2231b3c04886aa0da62f5dc03eda17d1e
Bennett Blodinger <bennett.blodinger@gmail.com>
2016-07-28 18:25:31 -0400

    IGNORE ME!

--- index.js
+++ index.js
@@ -16,10 +16,59 @@ const bot = controller.spawn({
 }).startRTM();
 const channel = process.env.PGO_SLACK_CHANNEL || 'general';
 
+function updateIgnoreList(id, remove) {
+    if (remove) {
+        ignoreList.map(function(object, index) {
+            if (object === id) {
+                ignoreList.splice(index, 1);
+            }
+        })
+    } else {
+        ignoreList.push(id);
+    }
+    ignoreList.sort();
+    controller.storage.channels.save({
+        'id': 'ignore',
+        'list': ignoreList
+    }, function(error, response) {
+        if (error) console.error(error);
+    });
+}
+
 // Set up bot commands
-controller.hears(['^ignore (\\d+)$'], ['direct_mention'], function(bot, message) {
-    let PokemonId = message.match[1];
-    bot.reply(message, `Alrighty, I'll ignore ${PokemonGO.pokemonlist[parseInt(PokemonId)-1].name} from now on`);
+controller.hears(['^(un|)ignore (\\d+)$'], ['direct_mention'], function(bot, message) {
+    let pokemonId = parseInt(message.match[2]);
+
+    // Check if it's a valid ID
+    if (PokemonGO.pokemonlist[parseInt(pokemonId)-1]) {
+        let pokemonName = PokemonGO.pokemonlist[parseInt(pokemonId)-1].name;
+
+        // Check if it is in the ignore list
+        if (ignoreList.includes(pokemonId)) {
+            if (message.match[1] === 'un') {
+                updateIgnoreList(pokemonId, true);
+                return bot.reply(message, `Okay, I'll watch out for ${pokemonName}.`);
+            } else {
+                return bot.reply(message, `I'm already ignoring ${pokemonName}.`);
+            }
+        } else {
+            if (message.match[1] === 'un') {
+                return bot.reply(message, `I'm already searching for ${pokemonName}.`);
+            } else {
+                updateIgnoreList(pokemonId)
+                return bot.reply(message, `Okay, I'll ignore ${pokemonName}.`);
+            }
+        }
+    } else {
+        return bot.reply(message, 'That Pokémon ID seems to be invalid.');
+    }
+});
+controller.hears(['^ignore list$'], ['direct_mention'], function(bot, message) {
+    let pokemonNames = [];
+    ignoreList.map(function(object, index) {
+        pokemonNames.push(PokemonGO.pokemonlist[parseInt(object)-1].name);
+    });
+    bot.reply(message, `I am currently ignoring ${pokemonNames.join(', ')}.`);
 });
 
 // Set up Pokémon GO API
@@ -42,7 +91,14 @@ const sprites = JSON.parse(fs.readFileSync(__dirname + '/sprites.json', 'utf8'))
 // Map base
 const mapBase = 'https://maps.googleapis.com/maps/api/staticmap?format=png32&zoom=17&size=400x300&&style=feature:administrative|visibility:off&style=feature:landscape.man_made|element:geometry.fill|color:0x89ff82&style=feature:landscape.man_made|element:labels|visibility:off&style=feature:poi|visibility:simplified&style=feature:poi|element:labels|visibility:off&style=feature:road|element:geometry.fill|color:0x808080&style=feature:road|element:geometry.stroke|color:0xedfe91&style=feature:road|element:labels|visibility:off&style=feature:transit|visibility:off&style=feature:water|element:geometry|color:0x1a8bd9&style=feature:water|element:labels|visibility:off&style=feature:poi.park|color:0x03a286&style=feature:poi&markers=icon:';
 
+// Initialize lists
 let seenList = [];
+let ignoreList = [];
+controller.storage.channels.get('ignore', function(error, response){
+    if (error) console.error(error);
+
+    ignoreList = response.list;
+});
 
 // Initalize the Pokémon GO API
 PokemonGO.init(username, password, location, provider, function(error) {
@@ -62,8 +118,8 @@ PokemonGO.init(username, password, location, provider, function(error) {
 
                 hb.cells.map(function(j) {
                     j.WildPokemon.map(function(k) {
-                        // Only alert for spawns not seen yet
-                        if (!seenList.some(function(element){ return element.EncounterId === Number(k.EncounterId) })) {
+                        // Only alert for spawns not seen and not part of the ignore list
+                        if (!seenList.some(function(element){ return element.EncounterId === Number(k.EncounterId) }) && !ignoreList.includes(k.pokemon.PokemonId)) {
 
                             // Add Pokémon to seen list.  This prevents duplicate notifications.
                             let timeLeft = moment.duration(k.TimeTillHiddenMs, 'milliseconds');
@@ -101,6 +157,7 @@ PokemonGO.init(username, password, location, provider, function(error) {
                             }
 
                         }
+
                         // Clean up the seen list
                         seenList.map(function(object, index) {
                             if (moment().isAfter(object.Expiration)) {
